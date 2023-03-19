dda27389c3ff37e8e0a41eb0621b0bec17dcbfba
Desmond Kofi Adusei <desdhi24@gmail.com>
2023-03-18 17:24:48 -0700

    variables

---
+++ c_lesson/variables.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("this is the begining\n");
+	int number = 0;
+	double number2 = 999999999999.99;
+	int total = number2;
+
+	printf("the number is %d:", total);
+	printf("number 2 is %f:", number2);
+	printf("\n");
+	return (0);
+}
